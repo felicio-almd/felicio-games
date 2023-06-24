a6c88fa48784dbc65e16cf8842de3b310c4fb9b5
Felicio Rocha <feliciorar@gmail.com>
2023-06-24 18:55:23 -0300

    create loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function Loader() {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
