6085c42463e8b9b8a98fca98264d63e6930c72e3
Felicio Rocha <feliciorar@gmail.com>
2023-06-24 18:57:05 -0300

    create features

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,150 @@
 import { Component, useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import logo from "./assets/logo-games-felicio.svg";
-import home from "./assets/home-button.svg";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import Search from "./components/Search";
+const LIMIT_GAMES = 12;
 
 function App() {
-  const [count, setCount] = useState(0);
   const [games, setGames] = useState([]);
+  const [search, setSearch] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("Todos");
+  const [filteredGames, setFilteredGames] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [limitGames, setLimitGames] = useState(LIMIT_GAMES);
 
-  function getGames() {
-    axios
-      .get("https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/", {
+  function getGenres() {
+    const uniqueGenres = [...new Set(games.map((game) => game.genre))];
+    setGenres(["Todos", ...uniqueGenres]);
+  }
+
+  async function getGames() {
+    try {
+      const response = await axios.get(import.meta.env.VITE_API_URL, {
         headers: {
-          "dev-email-address": "feliciorar@gmail.com",
+          "dev-email-address": "defensive00@gmail.com",
         },
         timeout: 5000,
-      })
-      .then((response) => {
-        console.log(response.data);
-        setGames((games) => (games = response.data));
-      })
-      .catch((error) => {
-        const errorCode = error.response.status;
-        if (
-          errorCode == 500 ||
-          errorCode == 502 ||
-          errorCode == 503 ||
-          errorCode == 504 ||
-          errorCode == 507 ||
-          errorCode == 508 ||
-          errorCode == 509
-        ) {
-          alert("O servidor fahou em responder, tente recarregar a página");
-        } else if (error.code === "ECONNABORTED") {
-          console.log("O servidor demorou para responder, tente mais tarde");
-        }
       });
+      setGames(response.data);
+    } catch (error) {
+      if (error.response) {
+        const status = error.response.status;
+        if (
+          status == 500 ||
+          status == 502 ||
+          status == 503 ||
+          status == 504 ||
+          status == 507 ||
+          status == 508 ||
+          status == 509
+        ) {
+          setIsError(true);
+          setErrorMessage(
+            "O servidor falhou em responder, tente recarregar a página"
+          );
+        }
+      } else if (error.code === "ECONNABORTED") {
+        setIsError(true);
+        setErrorMessage("O servidor demorou para responder, tente mais tarde");
+      } else {
+        setIsError(true);
+        setErrorMessage(
+          "O servidor não conseguirá responder por agora, tente voltar novamente mais tarde"
+        );
+      }
+    }
   }
+
+  const filterGames = (genre, search) => {
+    let filtered;
+    if (genre === "Todos" && search === "") {
+      filtered = games;
+    } else {
+      filtered = games.filter((game) => {
+        const matchesGenre = genre === "Todos" || game.genre === genre;
+        const matchesSearchTerm =
+          search === "" ||
+          game.title.toLowerCase().includes(search.toLowerCase());
+        return matchesGenre && matchesSearchTerm;
+      });
+    }
+    setFilteredGames(filtered);
+  };
+
+  const handleGenreChange = (event) => {
+    const genre = event.target.value;
+    setSelectedGenre(genre);
+    setLimitGames(LIMIT_GAMES);
+    filterGames(genre, search);
+  };
+
+  // Função para atualizar o estado do termo de pesquisa
+  const handleSearchChange = (event) => {
+    const searchTerm = event.target.value;
+    setSearch(searchTerm);
+    setLimitGames(LIMIT_GAMES);
+    filterGames(selectedGenre, searchTerm);
+  };
 
   useEffect(() => {
     getGames();
   }, []);
 
+  useEffect(() => {
+    filterGames(selectedGenre, search);
+  }, [games, selectedGenre, search]);
+
+  useEffect(() => {
+    getGenres();
+  }, [games]);
+
+  useEffect(() => {
+    function bottomPageVerify() {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        setLimitGames((limitGames) => limitGames + 12);
+      }
+    }
+    window.addEventListener("scroll", bottomPageVerify);
+    return () => {
+      window.removeEventListener("scroll", bottomPageVerify);
+    };
+  });
+
   return (
     <>
       <div className="body">
-        <div>
-          <header className="header">
-            <div className="header__logo">
-              <img
-                className="header__logo__image"
-                src={logo}
-                alt="logo-gamesfelicio"
-              />
-              <h1 className="header__logo__title">Games Felicio</h1>
-            </div>
-            <button className="header__home__button">
-              <img
-                className="header__home__logo"
-                src={home}
-                alt="home-button"
-              />
-            </button>
-            <input
-              type="text"
-              className="header__search__box"
-              name=""
-              placeholder="Digite aqui seu jogo..."
-            ></input>
-            <div className="games__genre">
-              <input type="radio" id="All" name="filtro" value="All" />
-              <label for="All">Todos</label>
-
-              <input type="radio" id="shooter" name="filtro" value="shooter" />
-              <label for="shooter">Tiro</label>
-
-              <input type="radio" id="RPG" name="filtro" value="RPG" />
-              <label for="RPG">RPG</label>
-
-              <input
-                type="radio"
-                id="Fighting"
-                name="filtro"
-                value="Fighting"
-              />
-              <label for="Fighting">Luta</label>
-
-              <input
-                type="radio"
-                id="battleRoyale"
-                name="filtro"
-                value="battleRoyale"
-              />
-              <label for="battleRoyale">Battle</label>
-
-              <input type="radio" id="battle" name="filtro" value="battle" />
-              <label for="battle">Batalha</label>
-            </div>
-          </header>
-        </div>
-
+        {games.length < 1 ? null : (
+          <Header
+            genres={genres}
+            onChange={handleGenreChange}
+            selectedGenre={selectedGenre}
+          >
+            <Search onChange={handleSearchChange} />
+          </Header>
+        )}
         <div className="games__container">
           {games.length < 1 ? (
-            <p>Ainda não existem jogos!</p>
+            <div>
+              {isError ? (
+                <p className="error-message"> {errorMessage} </p>
+              ) : (
+                <Loader />
+              )}
+            </div>
           ) : (
             <div className="games__main">
-              {games.map((game) => (
-                <a
-                  href={game.game_url}
-                  target="_blank"
-                  className="games__card"
-                  key={game.id}
-                >
-                  <img
-                    className="games__card__image"
-                    src={game.thumbnail}
-                    alt="thumb"
-                  />
-                  <div className="games__card__infos">
-                    <strong className="games__card__title">{game.title}</strong>
-                    <div className="games__sub__infos">
-                      <small className="games__card__genre">{game.genre}</small>
-                      <small className="games__card__dev">
-                        {game.developer}
-                      </small>
-                    </div>
-                  </div>
-                </a>
-              ))}
+              {filteredGames.length < 1 ? (
+                <div className="error-message">Nenhum Jogo Encontrado</div>
+              ) : (
+                filteredGames
+                  .slice(0, limitGames)
+                  .map((game) => <Card key={game.id} game={game} />)
+              )}
             </div>
           )}
         </div>
